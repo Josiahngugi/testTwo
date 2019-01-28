e37fc28fe4a512395864d05f750abdddb0a80c75
Josiahngugi <mainajosiahdev@gmail.com>
2019-01-28 03:19:28 -0800

    Promp the enteries from user

---
+++ src/main/java/Search.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Search {
+    public static void main(String [] args){
+
+        List collection = new ArrayList();
+        Scanner input= new Scanner(System.in);
+        System.out.println("please enter the of number items you want to insert");
+        int item= input.nextInt();
+    }
+}
