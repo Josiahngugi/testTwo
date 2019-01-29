1ad442a033783ab6a42c990834394ed8e51cd403
Josiah Maina <41941177+Josiahngugi@users.noreply.github.com>
2019-01-29 11:49:18 -0800

    adding throwables

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class Search {
     public static void main(String [] args){
-
+        
+        try{
         List collection = new ArrayList();
         Scanner input= new Scanner(System.in);
         System.out.println("please enter the of number items you want to insert");
@@ -30,6 +31,9 @@ public class Search {
         }
         else {
             System.out.println("-1");
+        }
+        }catch(Exception e){
+        System.out.println(e)
         }
     }
 }
